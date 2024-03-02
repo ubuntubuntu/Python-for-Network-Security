80b2ca48cb70ef6eb7760dfb9b8cae9e835bb26c
ubuntubuntu <157214183+ubuntubuntu@users.noreply.github.com>
2024-03-02 14:40:39 -0800

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,13 +1,14 @@
-protocol = {"ftp":21,"http":80,"ldap":389,"ssh":22}
+# "key" : value, key must be unique but value doesnt have to
+# we cannot change the key but we can change the value 
+protocol = {"ftp":21,"http":80,"ldap":389,"ssh":22} # the dictionary of protocols (key) and the port number (value)
 print(protocol)
-protocol["ftp"] = 210
+protocol["ftp"] = 210 # we assign new protocol number in ftp
 print(protocol) 
-keys = protocol.keys()
+keys = protocol.keys() # we extract the keys from dictionary and save it into keys 
 print(keys)
-ite = protocol.items()
+ite = protocol.items() # we extract the items from dictionary and save it into ite
 print(ite)
-ite.sort()
-for key,value in ite:
+for key,value in ite: 
     print(key,value)
 protocol["ldap"] = 389
 print(protocol)
