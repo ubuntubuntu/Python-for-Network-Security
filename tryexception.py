35e880e4eab460553974ec75dd6a54433338deb2
ubuntubuntu <157214183+ubuntubuntu@users.noreply.github.com>
2024-03-02 18:10:57 -0800

    read file

--- tryexception.py
+++ tryexception.py
@@ -2,3 +2,9 @@ try:
     print("10/0 :",str(10/0))
 except Exception as exception:
     print("Error :", str(exception))
+
+try:
+    f = open('fil.txt',"r")
+    print(f.read())
+except Exception as exception:
+    print("Error :", str(exception))
