eb5646233ea2ec2e4a67a36a0548083d9770027f
ubuntubuntu <157214183+ubuntubuntu@users.noreply.github.com>
2024-03-02 16:58:34 -0800

    add more class

--- practiceclassprotocol.py
+++ practiceclassprotocol.py
@@ -1,3 +1,7 @@
-from protocol import protocol
+from protocol import protocol,vulnerability
+
 protocol_http = protocol("HTTP", 80, "Hypertext Transfer Protocol")
-print("The protocol is : ", protocol_http.name,"The port is : ", protocol_http.number, "The description iis : " , protocol_http.description)
+print("The protocol is : ", protocol_http.name,"The port is : ", protocol_http.number, "The description is : " , protocol_http.description)
+
+vulnerability_http = vulnerability("Apache HTTP Server 2.4.50", "CVE 2021-42013", "Path Traversal & Remote Code Execution (RCE)")
+print("The vulnerability is : ", vulnerability_http.name,"The cve number is : ", vulnerability_http.cve, "The description is : " , vulnerability_http.description)
--- protocol.py
+++ protocol.py
@@ -1,5 +1,11 @@
-class protocol(object):
-    def __init__(self,name,number,description):
+class protocol(object): # we have to put word class to define that it is a class
+    def __init__(self,name,number,description): # its mandatory to put __init__ after word def
         self.name = name
         self.number = number
+        self.description = description
+
+class vulnerability(object):
+    def __init__(self,name,cve,description):
+        self.name = name
+        self.cve = cve
         self.description = description
